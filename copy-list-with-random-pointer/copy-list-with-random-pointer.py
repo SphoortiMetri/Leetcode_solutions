b62083f113cc4ecca03421be937932f79967c4bb
Sphoorti Metri <metrisphoorti@gmail.com>
2021-11-08 22:55:10 -0800

    Time: 36 ms (72.54%), Space: 15.1 MB (25.90%) - LeetHub

--- copy-list-with-random-pointer/copy-list-with-random-pointer.py
+++ copy-list-with-random-pointer/copy-list-with-random-pointer.py
@@ -11,20 +11,40 @@ class Solution:
     def __init__(self): 
         self.visitedHash = {}
         
+    def get_cloned_node(self, node):
+        if node: 
+            if node in self.visitedHash: 
+                return self.visitedHash[node]
+
+            new_node = Node(node.val, None, None) 
+            self.visitedHash[node] = new_node
+            return new_node
+        else: 
+            return None
+
+        
     def copyRandomList(self, head: 'Node') -> 'Node':
         if head == None: 
             return None 
         
-        if head in self.visitedHash: 
-            return self.visitedHash[head] 
+        old_node = head 
+        new_node = Node(old_node.val, None, None)
+        self.visitedHash[old_node] = new_node
         
-        node = Node(head.val, None,None)
-        self.visitedHash[head] = node
         
-        node.next = self.copyRandomList(head.next) 
-        node.random = self.copyRandomList(head.random) 
+        while old_node: 
+            new_node.random = self.get_cloned_node(old_node.random) 
+            new_node.next = self.get_cloned_node(old_node.next) 
+            
+            
+            
+            old_node = old_node.next 
+            new_node = new_node.next
+            
+        return self.visitedHash[head]
+            
         
-        return node
+   
 
         
         
