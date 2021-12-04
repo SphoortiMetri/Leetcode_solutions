e15fdac9e0e0f178738e0b4b805a1c7853496f2f
Sphoorti Metri <metrisphoorti@gmail.com>
2021-12-03 23:41:52 -0800

    Time: 992 ms (62.77%), Space: 16.7 MB (30.66%) - LeetHub

---
+++ minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.py
@@ -0,0 +1,31 @@
+class Solution:
+    def minInsertions(self, s: str) -> int:
+        
+        def dp(sI, eI, memo): 
+            
+            if sI > eI: 
+                return 0 
+            
+            if sI == eI: 
+                return 1 
+            
+            if memo[sI][eI] != -1: 
+                return memo[sI][eI] 
+            
+            #case 1 
+            if s[sI] == s[eI]: 
+                return 2 + dp(sI+1, eI-1, memo) 
+            
+            #case 2 
+            count1 = dp(sI+1,eI, memo) 
+            count2 = dp(sI, eI-1,memo) 
+            
+            res =  max(count1, count2) 
+            memo[sI][eI]  = res
+            return memo[sI][eI] 
+        
+        n = len(s)
+        
+        memo = [[-1 for i in range(n)] for j in range(n)]
+        return len(s) - dp(0, len(s)-1, memo)
+        
